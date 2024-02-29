3c29a108197e98d6bb1c3fa57341d5c876d97f95
DivyarajChudasama <92981225+DivyarajChudasama@users.noreply.github.com>
2024-03-01 00:05:59 +0530

    bugs

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,8 +1,5 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controller/user.controller');
-
-// Define the route with the correct controller method
 router.get('/test', userController.test);
-
 module.exports = router;
