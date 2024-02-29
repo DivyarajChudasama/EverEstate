fc69bbe725454bf53b76c921447b75ce22976137
DivyarajChudasama <92981225+DivyarajChudasama@users.noreply.github.com>
2024-03-01 00:04:24 +0530

    change

--- api/index.js
+++ api/index.js
@@ -1,25 +1,22 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const express=require('express')
+const mongoose=require('mongoose')
+const dotenv=require('dotenv')
 
-const userRouter = require('../api/routes/user.route');
-const authRouter = require('../api/routes/auth.route');
-dotenv.config();
-
-mongoose.connect(process.env.MONGO).then(() => {
+const userRouter=require('../api/routes/user.route')
+const authRouter=require('../api/routes/auth.route')
+dotenv.config()
+mongoose.connect(process.env.MONGO).then(()=>{
     console.log('Connected to MongoDB');
-}).catch((err) => {
-    console.error('Error connecting to MongoDB:', err);
-});
+}).catch((err)=>{
+    console.log(err)
+})
+const app=express();
 
-const app = express();
+app.use(express.json())
 
-app.use(express.json());
+app.listen(3000,()=>{
+    console.log("Server is running on port 3000")
+})
 
-app.use('/api/user', userRouter);
-app.use('/api/auth', authRouter);
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+app.use('/api/user',userRouter)
+app.use('/api/auth',authRouter)
