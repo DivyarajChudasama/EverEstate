489176a500d1c8204ca35870156a6c05cd9a6575
Yenish4894 <yenishradadiya@gmail.com>
2024-03-01 18:13:33 +0530

    created signup page

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,6 +1,7 @@
 const User=require('../models/user.model')
 const bcrypt=require('bcrypt')
-const signup=async (req,res)=> {
+const errorHandler=require('../utils/error')
+const signup=async (req,res,next)=> {
     const {username, email, password}=req.body;
     const hashedPassword= bcrypt.hashSync(password,10);
     const newUser = new User({username,email,password:hashedPassword});
@@ -11,7 +12,7 @@ const signup=async (req,res)=> {
     }
     catch(error)
     {
-        res.status(500).json(error.message);
+        next(error)
     }
     
 };
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Signin from './pages/Signin';
 import Signout from './pages/Signout';
 import Profile from './pages/Profile';
 import Header from './components/Header';
+import SignUp from './pages/SignUp';
 export default function App() {
   return <BrowserRouter>
   <Header/>
@@ -13,6 +14,7 @@ export default function App() {
     <Route path='/about' element={<About/>}/>
     <Route path='/signin' element={<Signin/>}/>
     <Route path='/signout' element={<Signout/>}/>
+    <Route path='/signup' element={<SignUp/>}/>
     <Route path='/profile' element={<Profile/>}/>
   </Routes>
   </BrowserRouter>
