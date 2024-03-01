9f0e0d7632f0e08df5dc9713aa861b984cf184ca
DivyarajChudasama <92981225+DivyarajChudasama@users.noreply.github.com>
2024-03-01 17:53:46 +0530

    error handler

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,6 +1,7 @@
 const User=require('../models/user.model.js')
 const bcrypt=require('bcrypt')
-const signup=async (req,res)=> {
+import { errorHandler } from '../utils/error.js';
+const signup=async (req,res,next)=> {
     const {username, email, password}=req.body;
     const hashedPassword= bcrypt.hashSync(password,10);
     const newUser = new User({username,email,password:hashedPassword});
@@ -11,7 +12,7 @@ const signup=async (req,res)=> {
     }
     catch(error)
     {
-        res.status(500).json(error.message);
+       next(errorHandler(550,'Error from the function'));
     }
     
 };
