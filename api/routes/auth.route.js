981d50b29bc5b68d3b144b7c5463210d4120b22e
Yenish4894 <yenishradadiya@gmail.com>
2024-03-03 14:28:38 +0530

    sign in api done

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,9 @@
 const express=require('express')
-const { signup } = require('../controller/auth.controller')
+const { signup, signin } = require('../controller/auth.controller')
 const { deflate } = require('zlib')
 const router=express.Router()
 router.post('/signup',signup)
+router.post('/signin',signin)
 
 // export default router
 module.exports=router
