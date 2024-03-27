91ec0589c25643ea389ac3ac8b71890c794f9d07
Yenish4894 <yenishradadiya@gmail.com>
2024-03-27 23:04:51 +0530

    header component updated with private profilepage functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import Signout from './pages/Signout';
 import Profile from './pages/Profile';
 import Header from './components/Header';
 import SignUp from './pages/SignUp';
+import PrivateRoute from './components/PrivateRoute'
 export default function App() {
   return <BrowserRouter>
   <Header/>
@@ -16,6 +17,7 @@ export default function App() {
     <Route path='/signout' element={<Signout/>}/>
     <Route path='/signup' element={<SignUp/>}/>
     <Route path='/profile' element={<Profile/>}/>
+    <Route element={<PrivateRoute/>}/>
   </Routes>
   </BrowserRouter>
 }
